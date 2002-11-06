5fe4c015786e331ff7f2ecbcf194296eefa6e68b
Steaphan Greene <steaphan@gmail.com>
2002-11-06 06:35:20 -0800

    Move next available .tar.gz backup into git

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
-#include <SDL/SDL.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
+// *************************************************************************
+// main.c
+// This is a component of Acid Rain, Pre ALPHA non-distribution version
+//
+// -By Insomnia (Steaphan Greene)   (Copyright 2002 Steaphan Greene)
+//                   (stea@core.binghamton.edu)
+//      No waranty stated or implied, I am not responsible for any damage
+// caused directly or indirectly by this software.
+//      Permision granted for use/distribution/modification by anyone,
+// provided this header remains intact, and modified versions are marked
+// as so immediately below this header.
+//      Products utilizing the code or methods within may be distributed
+// freely along with this licence, but any sales for profit of such products
+// must have the author's permission, and may be subject to a royaltee fee.
+// *************************************************************************
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- renderer.c
+++ renderer.c
@@ -1,3 +1,19 @@
+// *************************************************************************
+// renderer.c
+// This is a component of Acid Rain, Pre ALPHA non-distribution version
+//
+// -By Insomnia (Steaphan Greene)   (Copyright 2002 Steaphan Greene)
+//                   (stea@core.binghamton.edu)
+//      No waranty stated or implied, I am not responsible for any damage
+// caused directly or indirectly by this software.
+//      Permision granted for use/distribution/modification by anyone,
+// provided this header remains intact, and modified versions are marked
+// as so immediately below this header.
+//      Products utilizing the code or methods within may be distributed
+// freely along with this licence, but any sales for profit of such products
+// must have the author's permission, and may be subject to a royaltee fee.
+// *************************************************************************
+
 #include <SDL/SDL.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
@@ -392,6 +408,12 @@ void clicked(double x, double y, int b) {
       if(xtarg != xoff || ytarg != yoff) move = 0;
       }
     }
+  if(b == 4) {
+    ytarg -= 0.5;
+    move = 0;
+    }
+  if(b == 5) {
+    ytarg += 0.5;
+    move = 0;
+    }
   }
-
-
